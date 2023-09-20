380767a8c283df944e27293b674f4460b9c4e8c6
Dholhos <113104720+Dholhos@users.noreply.github.com>
2023-09-20 11:49:49 +0200

    Add files via upload

---
+++ Fuvar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga
+{
+    class Fuvar
+    {
+        int ID;
+        DateTime indulas;
+        int idotartam;
+        double tavolsag, veteldij,borravalo;
+        string fizetesModja;
+
+        public Fuvar(int iD, DateTime indulas, int idotartam, double tavolsag, double veteldij, double borravalo, string fizetesModja)
+        {
+            ID = iD;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tavolsag = tavolsag;
+            this.veteldij = veteldij;
+            this.borravalo = borravalo;
+            this.fizetesModja = fizetesModja;
+        }
+
+        public int ID1 { get => ID; set => ID = value; }
+        public DateTime Indulas { get => indulas; set => indulas = value; }
+        public int Idotartam { get => idotartam; set => idotartam = value; }
+        public double Tavolsag { get => tavolsag; set => tavolsag = value; }
+        public double Veteldij { get => veteldij; set => veteldij = value; }
+        public double Borravalo { get => borravalo; set => borravalo = value; }
+        public string FizetesModja { get => fizetesModja; set => fizetesModja = value; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Doga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Fuvar> fuvarok;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFaljBe_Click(object sender, RoutedEventArgs e)
+        {
+          OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileBeolvas(openFileDialog.FileName);
+            }
+        }
+        public void FileBeolvas(string Filename)
+        {
+            fuvarok = new List<Fuvar>();
+            foreach (var item in File.ReadAllLines(Filename))
+            {
+                int taxiID = int.Parse(item.Split(';')[0]);
+                DateTime taxiIndulas = DateTime.Parse(item.Split(';')[1]);
+                int taxiIdotartam = int.Parse(item.Split(';')[2]);
+                double taxiTavolsag = double.Parse(item.Split(';')[3]);
+                double taxiVetelDij = double.Parse(item.Split(';')[4]);
+                double taxiBorravalo = double.Parse(item.Split(';')[5]);
+                string taxiFizetesiModja = item.Split(';')[6];
+                fuvarok.Add(new Fuvar(taxiID, taxiIndulas, taxiIdotartam, taxiTavolsag, taxiVetelDij, taxiBorravalo, taxiFizetesiModja));
+            }
+        }
+
+        private void btnSzamol_Click(object sender, RoutedEventArgs e)
+        {
+            lblCount.Content ="3.feladat: "+ fuvarok.Count();
+        }
+
+        private void btnbevetel_Click(object sender, RoutedEventArgs e)
+        {
+            //Tanár úrat szeretném kérni hogy ezt is vegye figyelembe!
+            //lblBevetel.Content ="4.feladat: " +fuvarok.Sum(x => x.ID1 == 6185);
+        }
+
+        private void btnFizetesiMod_Click(object sender, RoutedEventArgs e)
+        {
+            
+            lbFizetésiModok.Items.Add(fuvarok.GroupBy(x => x.FizetesModja).Count());
+        }
+
+        private void btnMegtettTav_Click(object sender, RoutedEventArgs e)
+        {
+            lblMegtettTav.Content="6.feladat: "+ Math.Round(fuvarok.Sum(x => x.Tavolsag * 1.6),2);
+        }
+
+        private void btnLeghosszabb_Click(object sender, RoutedEventArgs e)
+        {
+            //Tanár úrat szeretném kérni hogy ezt is vegye figyelembe!
+           //lbLeghosszabb.Items.Add(fuvarok.MaxBy(x =>x.Idotartam));
+            
+        }
+    }
+}
